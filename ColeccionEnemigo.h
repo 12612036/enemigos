4b4c94abec977d0ca833c98f33e2a601c1904130
José Luis Uyuni <joseluisuyuni@gmail.com>
2025-06-11 07:58:00 -0400

    Update ColeccionEnemigo.h

--- ColeccionEnemigo.h
+++ ColeccionEnemigo.h
@@ -1,5 +1,5 @@
 #pragma once
-#include <vector>
+//#include <vector>
 #include <thread>
 #include <chrono>
 #include "Enemigo.h"
@@ -8,9 +8,12 @@ using namespace std;
 
 class CColeccionEnemigo {
 	vector<CEnemigo*> enemigos;
+	//std::vector<std::pair<int, int>> posEnemigos;
+	//std::vector<std::pair<int, int>> posLibres;
 
-public:
-	vector<CEnemigo*> getEnemigos();	
+public:	
+	vector<CEnemigo*> getEnemigos();
+	//void buscarEspacioLibre(const CanchaArray& cancha);
 	void crearEnemigo();	
 	void dibujar(Graphics^ g, Bitmap^ bmpEnemigo, const CanchaArray& cancha);
 };
