50058a10f4633242768c2297897d28b66453f8aa
José Luis Uyuni <joseluisuyuni@gmail.com>
2025-06-11 07:59:44 -0400

    Update ColeccionBomba.h

--- ColeccionBomba.h
+++ ColeccionBomba.h
@@ -8,10 +8,11 @@ using namespace std;
 class CColeccionBomba {
 	vector<CBomba*> bombas;
 	int totalBombas;
-
+	/*int CantidadBombas = 2;*/
 public:
 	CColeccionBomba();
-
+	//int getCantidadBombas();
 	void crearBomba(int x, int y);
 	void dibujarBomba(Graphics^ g, Bitmap^ bmpBomba, Bitmap^ bmpExplosion, int jugadorX, int jugadorY, const CanchaArray& cancha);
+	vector<CBomba*> getBombas();
 };
