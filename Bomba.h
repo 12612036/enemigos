c4a3301d71f44276cbc136d222b4d0c4debbed1a
José Luis Uyuni <joseluisuyuni@gmail.com>
2025-06-11 08:02:21 -0400

    Update Bomba.h

--- Bomba.h
+++ Bomba.h
@@ -7,7 +7,6 @@
 #define ANCHOEXPLOSION 20
 #define ALTOEXPLOSION 20
 #define TIEMPOEXPLOSION 6
-
 using namespace System::Drawing;
 
 enum EBomba {
@@ -21,6 +20,7 @@ class CBomba {
 	int y;
 	int indiceX;
 	int tiempoExplosion;
+	
 
 	EBomba estado;
 
@@ -32,9 +32,14 @@ public:
 
 	EBomba getEstado();	
 
-	void animarExplosion();		
-	bool verificarLugar(int jugadorX, int jugadorY, const CanchaArray& cancha);	
+	int getX();	
+	int getY();
+	void animarExplosion();
+	void getPosicionCancha(int jugadorX, int jugadorY, int &i, int &j);
+	bool verificarLugar(int i, int j, const CanchaArray& cancha);
 	void dibujar(Graphics^ g, Bitmap^ bmpBomba, int jugadorX, int jugadorY, const CanchaArray& cancha);		
 	void animar();
 	void dibujarExplosion(Graphics^ g, Bitmap^ bmpExplosion, const CanchaArray& cancha);	
 };
+
+
