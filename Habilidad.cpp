94a58159443a0b981277aed90262b5008b6aed30
José Luis Uyuni <joseluisuyuni@gmail.com>
2025-06-11 07:47:05 -0400

    Update Habilidad.cpp

--- Habilidad.cpp
+++ Habilidad.cpp
@@ -4,63 +4,88 @@
 #include "Habilidad.h"
 
 CHabilidad::CHabilidad() {
-    srand(time(NULL));
-    ubicado = false;
-    i = rand() % (FILAS - 2) + 1;
-    j = rand() % (COLUMNAS - 2) + 1;
-    indiceX = 0;
-    indiceY = 0;
-    tipoHabilidad = (rand() % 5) + 1;
-    tiempoInicio = time(NULL); // Guardar el tiempo de inicio
-    activo = true; // Habilidad activa inicialmente
-    visible = false; // Habilidad empieza oculta
+	srand(time(NULL));
+	ubicado = false;
+	i = rand() % (FILAS-2) + 1;
+	j = rand() % (COLUMNAS-2) + 1;
+	indiceX = 0;
+	indiceY = 0;
+	tipoHabilidad = (rand() % 5) + 1;
 }
-CHabilidad::~CHabilidad() {}
+
+CHabilidad:: ~CHabilidad() {};
+
+int CHabilidad::getHabilidad() {
+	return tipoHabilidad;
+}
 
 void CHabilidad::dibujar(Graphics^ g, Bitmap^ bmpHabilidad, const CanchaArray& cancha) {
-if (!activo || !visible) return; // No dibujar si la habilidad ha expirado o está oculta
+	Rectangle rectangulo = Rectangle(indiceX * ANCHOIMAGEN, indiceY * ALTOIMAGEN, ANCHOIMAGEN, ALTOIMAGEN);
 
-    Rectangle rectangulo = Rectangle(indiceX * ANCHOIMAGEN, indiceY * ALTOIMAGEN, ANCHOIMAGEN, ALTOIMAGEN);
-    while (!ubicado) {
-        if (cancha[i][j]->getTipo() == EElementos::rrompible) {
-            ubicado = true;
-        }
-        else {
-            if (j < COLUMNAS) {
-                i++;
-                if (i == (FILAS - 1)) {
-                    i = 0;
-                    j++;
-                }
-            }
-        }
-    }
+	while (!ubicado) {
+		if (cancha[i][j]->getTipo() == EElementos::rrompible) {
+			ubicado = true;
+		} else {
+			if (j < COLUMNAS) {
+				i++;
+				if (i == (FILAS-1)) {
+					i = 0;
+					j++;
+				}
+			}
+		}
+	}
 
-    Rectangle zoom = Rectangle(j * ANCHOIMAGEN, i * ALTOIMAGEN, ANCHOIMAGEN, ALTOIMAGEN);
-    g->DrawImage(bmpHabilidad, zoom, rectangulo, GraphicsUnit::Pixel);
+	Rectangle zoom = Rectangle(j * ANCHOIMAGEN, i * ALTOIMAGEN, ANCHOIMAGEN, ALTOIMAGEN);
+	g->DrawImage(bmpHabilidad, zoom, rectangulo, GraphicsUnit::Pixel);
 }
 
 void CHabilidad::animar() {
-    if (!activo) return; // No animar si la habilidad ha expirado
-
-    switch (tipoHabilidad) {
-    case 1: indiceX = EHabilidades::bomba; break;
-    case 2: indiceX = EHabilidades::fuego; break;
-    case 3: indiceX = EHabilidades::quitaVida; break;
-    case 4: indiceX = EHabilidades::velocidad; break;
-    case 5: indiceX = EHabilidades::vida; break;
-    }
-
-    indiceY = (indiceY + 1) % 2; // Animación sencilla
-
-    verificarDuracion(); // Comprobar si la habilidad debe desactivarse
-}
-//funcion para controlar la visibilidad de los habilidades
-void CHabilidad::mostrar() {
-    visible = true;
-}
-void CHabilidad::verificarDuracion() {
-    if (difftime(time(NULL), tiempoInicio) >= 10) {
-        activo = false; // Desactivar la habilidad después de 10 segundos
-    }
+	switch (tipoHabilidad) {
+	case 1: // Bomba
+		indiceX = EHabilidades::bomba;
+		if ((indiceY >= 0) && (indiceY < 1)) {
+			indiceY++;
+		}
+		else {
+			indiceY = 0;
+		}
+		break;
+	case 2: // Fuego
+		indiceX = EHabilidades::fuego;
+		if ((indiceY >= 0) && (indiceY < 1)) {
+			indiceY++;
+		}
+		else {
+			indiceY = 0;
+		}
+		break;
+	case 3: // Quitar vida
+		indiceX = EHabilidades::quitaVida;
+		if ((indiceY >= 0) && (indiceY < 1)) {
+			indiceY++;
+		}
+		else {
+			indiceY = 0;
+		}
+		break;
+	case 4: // Mayor velocidad
+		indiceX = EHabilidades::velocidad;
+		if ((indiceY >= 0) && (indiceY < 1)) {
+			indiceY++;
+		}
+		else {
+			indiceY = 0;
+		}
+		break;
+	case 5: // Aumentar vida
+		indiceX = EHabilidades::vida;
+		if ((indiceY >= 0) && (indiceY < 1)) {
+			indiceY++;
+		}
+		else {
+			indiceY = 0;
+		}
+		break;	
+	}
 }
