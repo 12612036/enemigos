36587b90e1df3d949e9cfbaa8a622ffd25a88686
José Luis Uyuni <joseluisuyuni@gmail.com>
2025-06-11 07:46:15 -0400

    Update Habilidad.h

--- Habilidad.h
+++ Habilidad.h
@@ -1,30 +1,25 @@
 #pragma once
 #include "Items.h"
 #include "Cancha.h"
-#include <ctime>
+
 using namespace System::Drawing;
+
 class CHabilidad {
-    int i;
-    int j;
+	int i;
+	int j;
 
-    int indiceX;
-    int indiceY;
-
-    int tipoHabilidad;
-    bool ubicado;
-    bool activo;        // Indica si la habilidad sigue activa
-    bool visible;       // Nueva variable para controlar si la habilidad es visible
-    time_t tiempoInicio; // Almacena el momento en que la habilidad se activa
+	int indiceX;
+	int indiceY;
+	
+	int tipoHabilidad;
+	bool ubicado;
+	int bombasMaximas;
 
 public:
-    CHabilidad();
-    ~CHabilidad();
+	CHabilidad();
+	~CHabilidad();
 
-    void dibujar(Graphics^ g, Bitmap^ bmpHabilidad, const CanchaArray& cancha);
-    void animar();
-    void verificarDuracion(); // Nueva función para controlar la duración
-    void mostrar(); // Nueva función para hacer visible la habilidad cuando sea necesario
-    bool esVisible() { return visible; } // Función para verificar visibilidad
+	void dibujar(Graphics^ g, Bitmap^ bmpHabilidad, const CanchaArray& cancha);
+	void animar();
+	int getHabilidad();
 };
-
-   
